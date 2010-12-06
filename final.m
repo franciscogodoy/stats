24bb8d03b8101d95a8f4fc1dfa7077b4055e5936
Thanh Doan <tcdoan@gmail.com>
2010-12-06 16:47:51 -0600

    reorg

--- final.m
+++ final.m
@@ -1,7 +1,50 @@
-% STAT 5531 - Multivariate statistical analysis
-% Final exam - problem 5.a
+% Final exam - problem 1 and 5
 % Thanh Doan 
 
+p1data = dataset('file','final1.data', 'delimiter',',');             
+X  = [p1data.X1, p1data.X2];
+[n,p] = size(X);
+
+xbar = mean(X)';
+S = cov(X);
+S_inv = inv(S);
+
+mu_prime = [6;12 ];
+T_sq = n*(xbar-mu_prime)'*S_inv*(xbar-mu_prime);
+
+critvalue = (p*(n-1)/(n-p)) * finv(.95,p,n-p);
+
+% The direction and lengths of axes of ellipsoid
+%   n * (xbar - mu)'*inv(S)*(xbar - mu) 
+%       <= c^2 = [p(n-1)/(n-p)]*F(.95, p, n-p)     
+% are:
+[v d] = eig(S);
+ellipse_center = xbar;
+disp(ellipse_center);
+for i = 1:size(d,2)
+    lambda = d(i,i);
+    half_len = sqrt(lambda) * sqrt( (p*(n-1)/(n*(n-p))) * finv(.95,p,n-p) );
+    fprintf('Axis %d, half-length = %10.4f, direction:  \n', i, half_len);
+    disp(v(:,i));    
+end
+
+theta = [0:0.1:360]';
+theta = theta .* (pi/180);
+a = sqrt(d(1,1))*sqrt(((p*(n-1))/(n*(n-p)))*finv(0.95,p,n-p)) .* v(:,1);
+b = sqrt(d(2,2))*sqrt(((p*(n-1))/(n*(n-p)))*finv(0.95,p,n-p)) .* v(:,2);
+for i = 1:length(theta)
+    x(i) = xbar(1) + a(1)*sin(theta(i)) + b(1)*cos(theta(i));
+    y(i) = xbar(2) + a(2)*sin(theta(i)) + b(2)*cos(theta(i));
+end
+
+plot(x,y);
+hold on
+axis equal
+plot(xbar(1),xbar(2),'r*');
+hold off
+
+
+% problem 5.a
 pollution_data = load('final5.data');
 
 % Compute principal components for the data, 
@@ -55,7 +98,7 @@ biplot(pc(:,1:3), 'scores',scores(:,1:3));
 axis([-1 1.1 -0.4 1 -1 1]);
 view([30 40]);
 
-
+% problem 5b - factor analysis
 [Loadings,specificVar,T,stats] = factoran(pollution_data,2,'rotate','none');
 
 %Ref: http://www.mathworks.com/help/toolbox/stats/brkgqnt.html
