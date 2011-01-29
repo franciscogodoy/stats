5bbeb46e8ba0c5c86af9cb2a93ddb4f48b3a8fd5
Thanh Doan <tcdoan@gmail.com>
2011-01-29 14:00:35 -0600

    Montgomery regressiob books datasets

---
+++ regression/Montgomery/Chapter 2/Examples/Example2.1.m
@@ -0,0 +1,28 @@
+% %example 2.1
+% Manual estimation of regression coefficients
+y = data(:,2);
+x = data(:,3);
+n = size(x,1);
+
+Sxx = sum(x.^2) - (sum(x)^2)/n;
+Sxy = dot(x,y) - (sum(x) * sum(y))/n;
+beta.hat1 = Sxy/Sxx;
+beta.hat0 = mean(y) - beta.hat1 * mean(x);
+
+%another way to estimate beta.hat1
+covxy =  cov(x,y);
+beta.hat1 = covxy(1,2)/var(x);
+
+% compute the residuals
+res = y - (beta.hat0 + beta.hat1 * x);
+
+SST = sum(y.^2) - (sum(y)^2)/n;
+SSE = SST - beta.hat1 * Sxy;
+sigma.hat.square = SSE/(n-2);
+
+
+
+% Use matlab stat toolbox
+X = [ones(n,1), x];
+[b,bint,r,rint,stats] = regress(y,X);
+
