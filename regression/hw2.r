2c7ecefa9ceb0d84b4c2e5dcca9c3f3e309724df
Thanh Doan <tcdoan@gmail.com>
2011-03-26 13:42:12 -0500

    hw2 R codes and docs

---
+++ regression/hw2.r
@@ -0,0 +1,131 @@
+	install.packages('car');
+	library(car);
+
+	b14 = read.table('b14.csv', sep=',', header=T);
+	attach(b14);
+	pairs(b14, pch=23, bg='orange', cex.labels=4, cex=2);
+	plot(b14[,c(1,2,3,4,6)], pch=23, bg='orange', cex=2);
+	plot(b14[,c(1,2,6)], pch=23, bg='orange', cex=2);
+	plot(b14[,c(3,4,6)], pch=23, bg='orange', cex=2);
+
+	n = nrow(b14);
+
+	m422= lm(y ~ x1+x2+x3+x4, data=b14);
+	summary(m422);
+	par(mfrow=c(1,2));
+	plot(m422, pch=23 ,bg='orange',cex=1);
+	par(mfrow=c(1,1));
+
+	e1 = rstandard(m422);
+	qqnorm(e1, main = "Q-Q Plot for the residuals"); qqline(e1, col = 'red');
+	identify(rep(1, length(y)), y, labels = seq_along(y));
+
+	plot(dfbetas(m422)[,'x1'], pch=23, bg='orange', cex=2, ylab="DFBETA (x1)");
+	plot(dfbetas(m422)[,'x3'], pch=23, bg='orange', cex=2, ylab="DFBETA (x3)");
+	par(mfrow=c(1,2));
+	plot(dfbetas(m422)[,'x2'], pch=23, bg='orange', cex=2, ylab="DFBETA (x2)");
+	plot(dfbetas(m422)[,'x4'], pch=23, bg='orange', cex=2, ylab="DFBETA (x4)");
+
+	plot(dffits(m422), pch=23, bg='orange', cex=2, ylab="DFFITS");
+
+	plot(cooks.distance(m422), pch=23,bg='orange', cex=2, ylab="Cook's distance");
+
+	# Bonferroni outlier test
+	outlierTest(m422);
+	influence.measures(m422);
+
+	subs = c(1,seq(3,25));
+	b14b = b14[subs,];
+	m422b=lm(y ~ x1+x2+x3+x4, data=b14b);
+	# old model 
+	m422;
+	# new model - without observation 2
+
+	m422b;
+	plot(m422b, pch=23 ,bg='orange',cex=2);
+
+	par(mfrow=c(1,2));
+	plot(dfbetas(m422b)[,'x2'], pch=23, bg='orange', cex=2, ylab="DFBETA (x2)");
+	plot(dfbetas(m422b)[,'x4'], pch=23, bg='orange', cex=2, ylab="DFBETA (x4)");
+
+	par(mfrow=c(1,1));
+	plot(dffits(m422b), pch=23, bg='orange', cex=2, ylab="DFFITS");
+	plot(cooks.distance(m422b), pch=23,bg='orange', cex=2, ylab="Cook's distance");
+
+	# Bonferroni outlier test
+	outlierTest(m422b);
+
+	#All the influence measures
+	influence.measures(m422b);
+
+	res = resid(m422b);
+
+	plot(predict(m422b), resid(m422b), pch=23,bg='orange', cex=2);
+	par(mfrow=c(1,2));
+	plot(predict(m422b), rstandard(m422b), pch=23,bg='orange', cex=1);
+	plot(predict(m422b), rstudent(m422b), pch=23,bg='orange', cex=1);
+	
+
+
+
+	m516b=lm(log(y) ~ x1+x2+x3+x4, data=b14b);
+	# model m516b- without obs 2 and use log transformation on y
+	m516b
+	plot(m516b, pch=23 ,bg='orange',cex=2);
+
+	par(mfrow=c(1,2));
+	plot(dfbetas(m516b)[,'x2'], pch=23, bg='orange', cex=2, ylab="DFBETA (x2)");
+	plot(dfbetas(m516b)[,'x4'], pch=23, bg='orange', cex=2, ylab="DFBETA (x4)");
+
+	par(mfrow=c(1,2));
+	plot(dfbetas(m516b)[,'x1'], pch=23, bg='orange', cex=2, ylab="DFBETA (x1)");
+	plot(dfbetas(m516b)[,'x3'], pch=23, bg='orange', cex=2, ylab="DFBETA (x3)");
+
+	par(mfrow=c(1,1));
+	plot(dffits(m516b), pch=23, bg='orange', cex=2, ylab="DFFITS");
+	plot(cooks.distance(m516b), pch=23,bg='orange', cex=2, ylab="Cook's distance");
+
+	# Bonferroni outlier test
+	outlierTest(m516b);
+
+	#All the influence measures
+	influence.measures(m516b);
+
+	res = resid(m516b);
+
+	# Added variable plots
+	par(mfrow=c(1,2));
+	avPlots(m516b, 'x1', pch=23,bg='orange', cex=2);
+	avPlots(m516b, 'x2', pch=23,bg='orange', cex=2);
+
+	par(mfrow=c(1,2));
+	avPlots(m516b, 'x3', pch=23,bg='orange', cex=2);
+	avPlots(m516b, 'x4', pch=23,bg='orange', cex=2);
+
+	#Hat values
+	plot(hatvalues(m516b), pch=23, bg='orange', cex=2, ylab='Hat values');
+
+	#Component+residual plots
+	par(mfrow=c(1,2));
+	crPlots(m516b, 'x1', pch=19, cex=2);
+	crPlots(m516b, 'x2', pch=19, cex=2);
+
+	par(mfrow=c(1,2));
+	crPlots(m516b, 'x3', pch=19, cex=2);
+	crPlots(m516b, 'x4', pch=19, cex=2);
+
+
+	# Now using log transformation both repressors and response variables.
+	m516c=lm(log(y) ~ log(x1)+log(x2)+log(x3)+log(x4), data=b14b);
+	m516c
+	plot(m516c, pch=23 ,bg='orange',cex=2);
+
+	# Bonferroni outlier test
+	outlierTest(m516c);
+
+	#All the influence measures
+	influence.measures(m516c);
+
+
+
+
--- regression/p2.19.r
+++ regression/p2.19.r
@@ -1,9 +1,61 @@
 
-x = seq(1,10.5,20);
+# Generare 500 samples. Each sample has 20 data points 
+# x = 1, 1.5,2,...,10.5 and y = 50 + 10x + e. where e ~ N(0,16)
+# For each sample, estimate slope, intercept, E(y|x), CIs 
+# 	then store these estimates in the estimates matrix
 
-# generare 500 samples 
+	n = 20;
+	x = seq(1,10.5,0.5);
+	estimates = matrix(0, nrow = 500, ncol=7, dimnames = list(c(1:500), 
+		c("beta0", "beta1", "meanY_x5", "slope.lwr", "slope.upr", "meanY.lwr", "meanY.upr")));
+
 	for (i in c(1:500))  {
-		e = 	rnorm(n, mean = 0, sd = 16);
+		e = rnorm(n, mean = 0, sd = 4);
 		y = 50 + 10*x + e;
-		
+		sample.lm = lm(y ~ x);
+		beta.hat = sample.lm$coef;
+		estimates[i,1:2] = beta.hat;
+		estimates[i,3] = sum(beta.hat * c(1,5));
+		estimates[i,4:5] = confint(sample.lm, 'x', level=0.95);
+            meanY.est = predict(sample.lm, list(x=5.00), interval='confidence', level=0.95);
+		estimates[i,6:7] =  meanY.est[2:3];
 	}
+
+# a) Now construct histogram of sample values of beta0.hat and beta1.hat
+	beta0.hat = estimates[,1];
+	hist(beta0.hat,main='Histogram of sample values of beta0', xlab='bata0');
+
+	beta1.hat = estimates[,2];
+	hist(beta1.hat,main='Histogram of sample values of beta1', xlab='bata1');
+
+
+# b)  Construct histogram of the estimates of E(y | x = 5) 
+	Ey_x5 =  estimates[,3];
+	hist(Ey_x5, main='Histogram of the estimates of E(y|x=5)', xlab='E(y|x=5)');
+
+# c)  95% CIs for beta1 was computed and stored in estimates[,4:5] previously 
+#     Here is code to check how many contains the true value beta1 = 10
+#       coverage_count count a number of CIs ontaining the true value beta1 = 10
+
+	total_samples = 500; 
+	coverage_count = sum(estimates[,4] <= 10 & 10<= estimates[,5]);
+	coverage_percent = coverage_count/total_samples;
+	data.frame(coverage_count , total_samples, coverage_percent);
+	
+
+# d)  95% CIs for E(y | x=5 ) was computed and stored in estimates[,6:7] previously 
+#     Here is code to check how many contains the true value of  E(y | x=5 ) = 100
+#       coverage_count count a number of CIs ontaining the true value
+
+	total_samples = 500; 
+	coverage_count = sum(estimates[,6] <= 100 & 100<= estimates[,7]);
+	coverage_percent = coverage_count/total_samples;
+	data.frame(coverage_count , total_samples, coverage_percent);
+	
+
+      
+
+
+
+
+		
