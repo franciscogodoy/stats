310cdfd00e0eb004a6eb4ff1ec55ec3c823d2093
Thanh Doan <tcdoan@gmail.com>
2011-11-28 15:08:14 -0600

    update numerical integration code to study the std error of 2 methods

--- simulation/MCIntegration2.m
+++ simulation/MCIntegration2.m
@@ -7,27 +7,41 @@ x = 0:0.01:pi;
 y = cos(x) .^ 2;
 plot(x,y)
 
-
 % Method 1: Based on simulation of n = 10,000 trials 
 X = rand(n,1);
 X = X .* pi;
 w = (cos(X) .^ 2) .* pi;
 I_est1 = sum(w)/n;
-fprintf('Numerical integration (method 1). I = %10.5f \n', I_est1);
-
+I_se = std(w)/sqrt(n);
+I_delta = I_se * norminv(0.975, 0,1);
+I_ci = I_est1 + [-1, 1] .* I_delta; 
+fprintf('I.hat = %10.5f \n', I_est1);
+disp('95 percent confidence inteval of I is ');
+disp(I_ci);
 
 % Method 2: 
+m = 100;
 xmax = pi;
 fmax = 1;
-n_accept = 0;
-for i=1:n
-  r1 = rand(1,1);
-  r2 = rand(1,1);
-  xr = r1*xmax;
-  fr = r2 * fmax;
-  if (fr < (cos(xr) ^ 2))
-    n_accept = n_accept +1;
+I_est = zeros(m,1);
+for k=1:m
+  n_accept = 0;
+  for i=1:n
+    r1 = rand(1,1);
+    r2 = rand(1,1);
+    xr = r1*xmax;
+    fr = r2 * fmax;
+    if (fr < (cos(xr) ^ 2))
+      n_accept = n_accept +1;
+    end
   end
+  I_est(k) = (n_accept/n) * (fmax * xmax);
 end
-I_est2 = (n_accept/n) * (fmax * xmax);
-fprintf('Numerical integration (method 2). I = %10.5f \n', I_est2);
+
+I_est2 = sum(I_est)/m;
+I_se2 = std(I_est);
+I_delta2 = I_se2 * norminv(0.975, 0,1);
+I_ci2 = I_est2 + [-1, 1] .* I_delta2; 
+fprintf('I.hat2 = %10.5f \n', I_est2);
+disp('95 percent confidence inteval of I is ');
+disp(I_ci2);
