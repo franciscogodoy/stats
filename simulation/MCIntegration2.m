917d9182caff9fff1d2cf8abd81de2420f7e5340
Thanh Doan <tcdoan@gmail.com>
2011-11-28 18:13:41 -0600

    update codes to disp 95% ci of the integration estimates

--- simulation/MCIntegration2.m
+++ simulation/MCIntegration2.m
@@ -1,5 +1,7 @@
-%% Part 1: Find integration of cos^2(x)dx from 0 to pi %%%%%%%%%%%%
+%% Find integration of cos^2(x)dx from 0 to pi %%%%%%%%%%%%
 
+clear;
+clc;
 n = 10^4;
 
 % Plotting 
@@ -7,7 +9,7 @@ x = 0:0.01:pi;
 y = cos(x) .^ 2;
 plot(x,y)
 
-% Method 1: Based on simulation of n = 10,000 trials 
+% Method 1: Ef(w(Xi))
 X = rand(n,1);
 X = X .* pi;
 w = (cos(X) .^ 2) .* pi;
@@ -15,12 +17,9 @@ I_est1 = sum(w)/n;
 I_se = std(w)/sqrt(n);
 I_delta = I_se * norminv(0.975, 0,1);
 I_ci = I_est1 + [-1, 1] .* I_delta; 
-fprintf('I.hat = %10.5f \n', I_est1);
-disp('95 percent confidence inteval of I is ');
-disp(I_ci);
 
 % Method 2: 
-m = 100;
+m = 1000;
 xmax = pi;
 fmax = 1;
 I_est = zeros(m,1);
@@ -42,6 +41,12 @@ I_est2 = sum(I_est)/m;
 I_se2 = std(I_est);
 I_delta2 = I_se2 * norminv(0.975, 0,1);
 I_ci2 = I_est2 + [-1, 1] .* I_delta2; 
+
+fprintf('I.hat = %10.5f \n', I_est1);
 fprintf('I.hat2 = %10.5f \n', I_est2);
-disp('95 percent confidence inteval of I is ');
+
+disp('Method 1: 95 percent confidence inteval of I ');
+disp(I_ci);
+
+disp('Method 2: 95 percent confidence inteval of I ');
 disp(I_ci2);
